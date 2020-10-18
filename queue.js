3dea193965cf45b85e4c47e3c17206e621c1d99c
rokydas <droky136@gmail.com>
2020-10-18 17:14:48 +0600

    stack and queue added

---
+++ queue.js
@@ -0,0 +1,21 @@
+class Queue{
+    constructor(){
+        this.queue = []; 
+    }
+    enqueue(item){
+        this.queue.push(item);
+    }
+    dequeue(){
+        if(this.queue.length){
+            return this.queue.shift();
+        }
+    }
+}
+
+const premiks = new Queue();
+premiks.enqueue('Rahman');
+premiks.enqueue('Jalil');
+premiks.enqueue('Malil');
+console.log(premiks.queue);
+const winner = premiks.dequeue();
+console.log(winner, premiks.queue);
--- stack.js
+++ stack.js
@@ -1,1 +1,20 @@
-// sfsdf
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+    add(item) {
+        this.stack.push(item);
+    }
+    remove() {
+        if(this.stack.length) {
+            return this.stack.pop();
+        }
+    }
+}
+
+const guest = new Stack();
+guest.add('Rahman');
+guest.add('Roky');
+console.log(guest.stack);
+const speaker = guest.remove();
+console.log(speaker, guest.stack);
